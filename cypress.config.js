70863c280ab5fd714b4de80f6fa96c2542fe90fe
Dipendra Shakya <shakyadipen@gmail.com>
2025-06-08 15:59:32 +0545

    Cypress Automation Framework

---
+++ cypress.config.js
@@ -0,0 +1,51 @@
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+
+  reporter:'cypress-mochawesome-reporter',
+reporterOptions:{
+  mochaFile:'results/my-test-output.xml',
+  reportPageTitle:'Cypress Intuji-Challenge-Report',
+  toConsole:true,
+  charts:true,
+  overwrite:false,
+  embeddedScreenshots:true
+},
+  //
+  e2e: {
+    
+    testIsolation:false,
+    watchForFileChanges:false,
+    chromeWebSecurity:false,
+    viewportWidth:1000,
+    viewportHeight:600,
+    waitForAnimations:true,
+    animationDistanceThreshold:30,
+    defaultCommandTimeout:6000,
+    execTimeout:60000,
+    pageLoadTimeout:60000,
+    requestTimeout:15000,
+    responseTimeout:15000,
+    video:true,
+    failOnStatusCode:false,
+    
+
+    setupNodeEvents(on,config) {
+      // implement the test run pattern
+     // addMatchImageSnapshotPlugin(on,config);
+      //require('@simonsmith/cypress-image-snapshot/plugin')(on);
+      //return getCompareSnapshotsPlugin(on, config);
+      require('cypress-mochawesome-reporter/plugin')(on);
+     
+    },
+  },
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,51 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+import loginPage from "../pageObjects/LoginPage.cy";
+import productPage from "../pageObjects/ProductPage.cy";
+Cypress.Commands.add('login', (email, password) => {
+    const loginObj=new loginPage()
+    loginObj.userLogin(email,password)
+
+})
+
+Cypress.Commands.add('AddToCart',(targetProductID)=>{
+    const uniqueProductIds = new Set();
+    //const targetProductID=['16','33'];
+    const productObj=new productPage()
+
+    productObj.provideAddToCartDetails().each(($el)=>{
+
+        const productId = $el.attr('data-product-id');
+        
+                  
+        if(targetProductID.includes(productId)&& !uniqueProductIds.has(productId)){
+            uniqueProductIds.add(productId);
+           
+            cy.wrap($el).first().click({force:true})
+            productObj.clickContinueLink()
+        }
+      
+    })
+})
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
